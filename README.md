289ec2d8940e92ee25b61f369364b2037d12ffde
coldmund <coldmund@gmail.com>
2020-01-02 23:39:38 +0900

    parse XMP in jpeg file

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## purpose
 * Recent LG smart phones have a feature "Live Photo" that works 'almost' like iPhone's but with a big flaw. You can play short movie of your photo on you phone, but you cannot find any movie file. (iPhone makes 1 still image file and 1 movie file for 1 shutter click)
 * I found that LG mobile phone saves the movie(MP4) in jpeg file after EOI marker and it makes jpeg file larger.
-* The purpose of this project is to extract the movie data and saves jpeg and mp4 separately.
+* The purpose of this project is to extract the movie data then to save jpeg and mp4 separately.
 
 ## usage
 `python3 main.py [-h|--help] [-r|--recursive] [-t|--target=<path>]`
--- main.py
+++ main.py
@@ -1,35 +1,91 @@
-import	argparse
+import argparse
 from pathlib import Path
+import xml.etree.ElementTree as ET
 
 def	extIsJpeg(file):
 	return	file.name.endswith('jpg') or file.name.endswith('jpeg')
 
+def	read2Bytes(file):
+	data = file.read(2)
+	if data:
+		return	int.from_bytes(data, byteorder='big')
+	else:
+		raise EOFError
+
 # return:
+#	0: OK
 #	-1: not a jpeg file
-#	-2: no exif
-#	-3: no XMD
-#	-4: no movie data
+#	-2: no XMP
+#	-3: XMP error
 def	processFile(file):
 	print("processFile: {}".format(file.name))
-	# 1. check jpeg(SOI)
 
-	# 2. check exif
+	with open(file, 'rb') as f:
+		# 1. check jpeg SOI
+		if read2Bytes(f) != 0xffd8:
+			print('	SOI not found')
+			return	-1
 
-	# 3. find XMP
+		# 2. search for app1 marker
+		xmpFound = False
+		xmpStr = ''
+		try:
+			while xmpFound == False:
+				while read2Bytes(f) != 0xffe1:
+					pass
+				# 3. search for XMP
+				size = read2Bytes(f)
+				data = ''
+				try:
+					data = f.read(size-2).decode('ascii')
+					xmpStart = data.find('<x:xmpmeta')
+					xmpEnd = data.find('</x:xmpmeta>')
+					if xmpStart != -1 and xmpEnd != -1:
+						xmpFound = True
+						xmpStr = data[xmpStart:xmpEnd+12]
+						# print('xmpStr: ', xmpStr)
+				except:
+					# print('	not ascii text')
+					pass
+		except EOFError:
+			print('EOF')
+			return	-2
 
-	# 4. save files
+		# 4. parse xmp
+		xmp = ET.fromstring(xmpStr)
+		nmspdict = {
+			'x': 'adobe:ns:meta/',
+            'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+            'LGLivePic': 'http://ns.lge.com/gallery/1.0/livepicture/',
+			'xmpNote': 'http://ns.adobe.com/xmp/note/',
+			'LGBehindVideo': 'http://ns.lge.com/gallery/1.0/behindvideo/'}
+		# print('xmp: ', ET.tostring(xmp))
+		version = xmp.find('rdf:RDF/rdf:Description/LGLivePic:Version', namespaces=nmspdict)
+		mime = xmp.find('rdf:RDF/rdf:Description/LGBehindVideo:Mime', namespaces=nmspdict)
+		nOffset = xmp.find('rdf:RDF/rdf:Description/LGBehindVideo:NegativeOffset', namespaces=nmspdict)
+		size = xmp.find('rdf:RDF/rdf:Description/LGBehindVideo:Size', namespaces=nmspdict)
+		if version is None or mime is None or nOffset is None or size is None:
+			print('xmp error')
+			return	-3
+		nOffset = int(nOffset.text)
+		size = int(size.text)
+		# print('nOffset: {}, size: {}'.format(nOffset, size))
+
+		# 5. save files
+
+		return	0
 
 def	processDirectory(p):
 	# print("processDir: {}".format(p.name))
 	[processFile(file) for file in list(p.glob('*')) if file.is_file() and extIsJpeg(file)]
 	[processDirectory(dir) for dir in p.iterdir() if dir.is_dir()]
 
-def	main(recursive, path):
-	print("main: {}, {}".format(recursive, path))
-	if path==None:
-		path = "."
+def	main(recursive, target):
+	print("main: {}, {}".format(recursive, target))
+	if target==None:
+		target = "."
 
-	p = Path(path)
+	p = Path(target)
 	if p.is_file():
 		processFile(p)
 	elif recursive==True:
@@ -44,4 +100,4 @@ if __name__ == "__main__":
 
 	args = parser.parse_args()
 
-	main(args.recursive, args.path)
+	main(args.recursive, args.target)
